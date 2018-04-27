2cb790f9f660b0d6d64b5b06f3cc225322f88c35
huang-br <brhuang000@gmail.com>
2018-04-26 21:21:09 -0700

    Adding a Least Square model

---
+++ Code/productionMode/LSmodel.py
@@ -0,0 +1,62 @@
+import os
+import sys
+import math
+import reader
+import numpy
+import matplotlib.pyplot as plt
+
+def LSmodel(pointsList):
+	#initializing variables
+	xSum = 0
+	xSquareSum = 0
+	ySum = 0
+	ySquareSum = 0
+	mulSum = 0
+	numerator = 0
+	denominator = 0
+	slope = 0
+	intercept = 0
+
+	xpoints = list()
+	ypoints = list()
+
+	#calculating slope
+	for point in pointsList:
+		mulSum += (point[0] * point[1])
+		xSum += point[0]
+		xSquareSum += (point[0] * point[0])
+		ySum += point[1]
+		ySquareSum += (point[1] * point[1])
+		xpoints.append(point[0])
+		ypoints.append(point[1])
+
+	numerator = mulSum - ((xSum * ySum)/len(pointsList))
+	denominator = xSquareSum - (pow(xSum, 2)/len(pointsList))
+	#print(numerator)
+	#print(denominator)
+	slope = float(numerator)/float(denominator)
+
+	#calculating intercept
+	intercept = (ySum - (slope * xSum))/len(pointsList)
+	
+	#calculating R^2
+	rNumerator = (len(pointsList) * float(mulSum)) - ((float(xSum) * float(ySum)))
+	rDenominator = math.sqrt(((len(pointsList) * xSquareSum) - (xSum * xSum)) * ((len(pointsList) * ySquareSum) - (ySum * ySum)))
+	r = rNumerator/rDenominator
+	rsquare = (r * r)
+	#print "r^2:",rsquare 
+
+	#creating a linear line for the graph
+	x = numpy.linspace(35,38,50)
+	y = (slope * x) + intercept
+
+	#Creating the scatter plot
+	#plt.scatter(xpoints, ypoints)
+	#plt.xlabel("Camera Temperatures")
+	#plt.ylabel("Thermometer Temperatures")
+	#plt.plot(x,y)
+	#plt.show()
+		
+	return [slope, intercept]
+
+def equation
