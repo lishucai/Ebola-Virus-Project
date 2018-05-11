391e1dd5c41e2cd8baa8b94305746020248a018e
maimonc <maimonc@oregonstate.edu>
2018-05-10 19:40:09 -0700

    Adding a README and adding final comments to images2.py.

--- Code/Image_processing/README.md
+++ Code/Image_processing/README.md
@@ -1,2 +1,15 @@
-Images_statistics is the code that we used to learn about opencv. It is the first version of our code. 
-images1.2 is a more advanced code. It gets all the files from a folder, and checks the file type. If it's a tiff file it makes some image processing, if it's a csv it gets the value, creat a histogram and print some statistics data. 
+## images2.py
+
+This program can be used to process a CSV file and produce a mean value from it. The CSV should hold the temperature data from a thermal image.
+The temperatures should be in Celsius. The program will find where the head of the person in the image begins and produce the mean of the head values.
+
+
+## To run the code
+
+You can either hard code a folder of CSV files in the code or you can define the folder as a command line argument. If you supply a folder at run time the code will use the folder specified in the code.
+
+Run: python images2.py <optional folder path>
+
+## Output
+
+The code produces a CSV file that holds all the mean values of the CSV files in the folder. In DEBUG mode the code doesn't create a file but it prints out the mean values of each file followed by the name of the file. This can help when trying to train the model. Printing the filename would help to match the calculated temperature with the temperature given by the thermometer.
--- Code/Image_processing/images2.py
+++ Code/Image_processing/images2.py
@@ -4,29 +4,36 @@ from os import listdir  # to get all files in a directory
 from os.path import isfile, join
 import os
 import sys
-#import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import csv
 
+#this function is used to correct the values in the CSV file. It finds the minimum values in
+# the double list and substruct it from all the values in the list. This is done to help with the calibration
+#of the camera.
 def correcting_values(temperatureData):
-
+    #setting intitial values so everythign would be smaller
     min_value = 1000
+    #goes through the list to fin the absolute minimum
     for elements in temperatureData:
         temp_min_value = min(elements)
         if temp_min_value < min_value:
             min_value = temp_min_value
-    #min_value = min(temperatureData)
+    # min_value = min(temperatureData)
     if DEBUG:
         print min_value
+    #substruct the minimum value from all values
     for row_counter, elements in enumerate(temperatureData):
         for column_counter, element in enumerate(elements):
             element = element - min_value
             temperatureData[row_counter][column_counter] = element
-            #[float(i)-min_value for i in elements]
-            #print temperatureData[row][column]
-
+            # [float(i)-min_value for i in elements]
+            # print temperatureData[row][column]
+    #returns the new list with changed values
     return temperatureData
 
-
+#The following funciton finds the point where the head begings. It does that be looking throught the array until it
+#finds five values in a row that are in the right range (30-45) then it returns a list with two variables [y,x]
+# y is the row where the ead begins and x is the column. If it doesn't find the head it returns an empty list
 
 def starting_point(temperatureData):
     for row_counter, elements in enumerate(temperatureData):
@@ -39,7 +46,7 @@ def starting_point(temperatureData):
                         if elements[column_counter + iterator] < 45 and elements[column_counter + iterator] > 30:
                             # print "added to good"
                             good.append(iterator)
-                #print len(good)
+                # print len(good)
                 if len(good) == 4:
                     # print "in"
                     cordinates = []
@@ -47,6 +54,8 @@ def starting_point(temperatureData):
                     cordinates.append(column_counter)
                     return cordinates
     return []
+
+#this function does the same as the previous just with a differernt range in case the file is bad.
 
 def starting_point_bad_file(temperatureData):
     for row_counter, elements in enumerate(temperatureData):
@@ -59,7 +68,7 @@ def starting_point_bad_file(temperatureData):
                         if elements[column_counter + iterator] < 50 and elements[column_counter + iterator] > 30:
                             # print "added to good"
                             good.append(iterator)
-                #print len(good)
+                # print len(good)
                 if len(good) == 4:
                     # print "in"
                     cordinates = []
@@ -69,8 +78,15 @@ def starting_point_bad_file(temperatureData):
     return []
 
 
-def get_mean(x, y, WIDTH, HIGHT, temperatureData):
-    for row in range(y, (y + HIGHT)):
+#This function returns the mean of the head values.
+#It recieves the following:
+# X and Y the values from the starting point function
+#WIDTH AND HEIGHT - variable that are hard coded from absorbing the images. If the person is about 2 meters from
+#the camera the head in the picture is about 40 columns wide and 30 rows long
+#temperatureData is the array with the temperature values
+
+def get_mean(x, y, WIDTH, HEIGHT, temperatureData):
+    for row in range(y, (y + HEIGHT)):
         # y is the row it begains, h is the width of the length of the head
         for column in range(x - (WIDTH / 2), x + (WIDTH / 2)):
             if (temperatureData[row][column] > 43 or temperatureData[row][column] < 35):
@@ -81,9 +97,10 @@ def get_mean(x, y, WIDTH, HIGHT, temperatureData):
     range_mean = sum(good_range) / float(len(good_range))
     return range_mean
 
+#this function does the same as the previous just with a differernt range in case the file is bad.
 
-def get_mean_bad_file(x, y, WIDTH, HIGHT, temperatureData):
-    for row in range(y, (y + HIGHT)):
+def get_mean_bad_file(x, y, WIDTH, HEIGHT, temperatureData):
+    for row in range(y, (y + HEIGHT)):
         # y is the row it begains, h is the width of the length of the head
         for column in range(x - (WIDTH / 2), x + (WIDTH / 2)):
             if (temperatureData[row][column] > 50 or temperatureData[row][column] < 30):
@@ -94,77 +111,104 @@ def get_mean_bad_file(x, y, WIDTH, HIGHT, temperatureData):
     range_mean = sum(good_range) / float(len(good_range))
     return range_mean
 
+if __name__ == '__main__':
+    '''
+       The following program processes a CSV file and outputs one float values.
+       The CSV file should hold temp values and should not be empty.
+       The program trasfers the CSV file into a double list and then processes the information in the list.
+       Overall it finds the head of the person in the csv file and averages the temperatures that belong to the head.
+       the directory should hold the csv files that you want to process
+       '''
 
-#MYPATH = 'C:\Users\Tair\Documents\Pictures\Real\Running'
-#MYPATH = 'nfs/stak/users/maimonc/Ebola-Virus-Project/Code/Image_processing\Real\Running'
-MYPATH = './Real/resting' 
-FILES_NUM = 0
-HIGHT = 30
-WIDTH = 40
-DEBUG = 0
-FILE_IS_GOOD = 1
-y = 0
-x = 0
-csv_list = []
-tiff_list = []
-invalid_file_type = []
-# getting the names of all the files in a directory
-getfiles = [f for f in listdir(MYPATH) if
-            isfile(join(MYPATH, f))]  # a list of all the file in a folder specified by MYPATH
+    # can set the file name in the command lind, just need to uncomment this MYPATH and comment out the hard coded one.
+    if (len(sys.argv) == 1):
+        MYPATH = 'C:\Users\Tair\Documents\Pictures\Real\Resting'
+    elif (len(sys.argv)==2):
+        MYPATH = str(sys.argv[1])
+    
+    #MYPATH = './Real/resting'
+    FILES_NUM = 0
+    # we hard coded the size of the head in the image. If the person is about 2 meter from the camera its about 30
+    # rows long and about 40 columns wide. You should change these values if you think the head is in a different
+    # size in you images.
+    HEIGHT = 30
+    WIDTH = 40
+    # set DEBUG to 1 to use specific DEBUG functionalities. It will have more print statments and will not print a
+    # single number
+    DEBUG = 0
 
-for f in getfiles:
-    if (f.endswith(".tiff")):  # Add more if statements as necessary for more file types
-        tiff_list.append(f)
-    elif (f.endswith(".csv")):
-        csv_list.append(f)
-    else:
-        invalid_file_type.append(f)
+    #set this variable to 1. I set it to 0 when we took some images that we'rent in the right range. It increases the range when selecting
+    #the head pixels. See  starting_point_bad_file() get_mean_bad_file
+    FILE_IS_GOOD = 1
 
-for c in csv_list:
-    # print c
-    temp_file = join(MYPATH, c)  # getting the file with the path, file name is in c
-    temperatureData = list()
-    tempList = []
-    with open(temp_file, 'rb') as f:
-        data = csv.reader(f, delimiter=',')
-        for row in data:
-            tempList = []
-            for temp in row:
-                if (temp == '\n'):
-                    pass
-                else:
-                    tempList.append(float(temp))
-            temperatureData.append(tempList)
-    temperatureData = correcting_values(temperatureData)
-    # print len(temperatureData[0])
-    good_range = []
+    #setting all nessesery parameters. x and y will hold the point where the head begins
+    y = 0
+    x = 0
+    csv_list = []
+    tiff_list = []
+    invalid_file_type = []
 
-    # y = 15
-    # x = 75
-    cordinates = []
-    cordinates = starting_point(temperatureData)
-    if not cordinates:
-        print "error! list of cordinates is empty"
+    # getting the names of all the files in a directory
+    getfiles = [f for f in listdir(MYPATH) if
+                isfile(join(MYPATH, f))]  # a list of all the file in a folder specified by MYPATH
+    #going over all the files in the directory. Putting all the CSV file names in one list and tiff file names in
+    #another. We don't do anything with the tiff files but we left it incase someone want's to process them too.
+    for f in getfiles:
+        if (f.endswith(".tiff")):  # Add more if statements as necessary for more file types
+            tiff_list.append(f)
+        elif (f.endswith(".csv")):
+            csv_list.append(f)
+        else:
+            invalid_file_type.append(f)
+    # a loop that goes through all the csv file. It will process each csv file by itself.
+    for c in csv_list:
+        temp_file = join(MYPATH, c)  # getting the file with the path, file name is in c
+        temperatureData = list()
+        tempList = []
+        #open csv file for read and trasnfer it into a two dimentional array. Each row is a list.
+        # And the temperatureData list holds a list of the rows.
+        with open(temp_file, 'rb') as f:
+            data = csv.reader(f, delimiter=',')
+            for row in data:
+                tempList = []
+                for temp in row:
+                    if (temp == '\n'):
+                        pass
+                    else:
+                        tempList.append(float(temp))
+                temperatureData.append(tempList)
+        temperatureData = correcting_values(temperatureData)
+        # print len(temperatureData[0])
+        good_range = []
 
-    if DEBUG:
-        print 'row number is %d, column number is %d ' %(cordinates[0], cordinates[1])
+        # y = 15
+        # x = 75
+        cordinates = []
 
-    y = cordinates[0]
-    x = cordinates[1]
+        #cordinate will hold a list of two variables. y which is the hight of where the head begins and x which is the column
+        # where the head begins
+        cordinates = starting_point(temperatureData)
+        if not cordinates:
+            print "error! list of cordinates is empty"
 
-    if FILE_IS_GOOD:
-        range_mean = get_mean(x,y,WIDTH, HIGHT, temperatureData)
-    else:
-        range_mean = get_mean_bad_file(  x,y,WIDTH, HIGHT, temperatureData)
+        if DEBUG:
+            print 'row number is %d, column number is %d ' % (cordinates[0], cordinates[1])
 
+        y = cordinates[0]
+        x = cordinates[1]
 
+        #if you set FILE_IS_GOOD to 1, it will use the normal get_mean. If you set it to 0 it would use the bad file get mean
+        #which uses a different range.
+        if FILE_IS_GOOD:
+            range_mean = get_mean(x, y, WIDTH, HEIGHT, temperatureData)
+        else:
+            range_mean = get_mean_bad_file(x, y, WIDTH, HEIGHT, temperatureData)
 
-    if DEBUG:
-        print 'mean: %f range_mean %s' % (range_mean, c)
-    else:
-       with open('resting.csv', 'a') as file:
-	     file.write(str(range_mean))
-	     file.write('\n')
-        #print '%0.2f %s' %(range_mean, c)
-
-
+        if DEBUG:
+            print 'mean: %f range_mean %s' % (range_mean, c)
+        else:
+            #prints the calculated mean into a CSV file.
+            with open('resting.csv', 'a') as file:
+                file.write(str(range_mean))
+                file.write('\n')
+            # print '%0.2f %s' %(range_mean, c)
